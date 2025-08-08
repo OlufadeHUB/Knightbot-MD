5d2c44b42cf6aed60bffb35593831b86c46d38c5
OlufadeHUB <carolineolufade8@gmail.com>
2025-08-08 22:12:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Chaos Bot",
   botOwner: 'King😈👑', // Your name
-  ownerNumber: '2348136025125', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2348033654467', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
